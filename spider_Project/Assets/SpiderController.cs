cd72faab16b2f32db9e65e9dbc01f102e1b380a2
arnav444a <93905434+arnav444a@users.noreply.github.com>
2021-11-08 23:02:22 +0530

    Project uploaded
    Spider animation projects unploaded

---
+++ spider_Project/Assets/SpiderController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderController : MonoBehaviour
+{
+    private Animator myAnimator;
+
+    private void Start()
+    {
+        myAnimator = GetComponent<Animator> ();
+
+    }
+
+    private void Update()
+    {
+        if (myAnimator)
+        {
+            myAnimator.SetFloat("speed", Input.GetAxis("Vertical"));
+        }
+    }
+}
